c8d1be7d0f01d06f0434e4d9cfa8293e8c3656e4
rodrigomirazo <rodrigomirazo@gmail.com>
2025-09-11 16:55:38 -0600

    first commit

---
+++ src/main/java/com/springboot/kafka_trainning/KafkaTrainningApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.kafka_trainning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTrainningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTrainningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/kafka_trainning/KafkaTrainningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.kafka_trainning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTrainningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
